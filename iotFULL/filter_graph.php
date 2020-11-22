9cf65914cdeefb4cf2343e22b7a25342905cfd56
11900456 <61419200+11900456@users.noreply.github.com>
2020-11-22 19:41:13 +0100

    Add files via upload

--- iotFULL/filter_graph.php
+++ iotFULL/filter_graph.php
@@ -1,5 +1,5 @@
 <?php
-	if(isset($_POST["from_date"], $_POST["to_date"])) {
+/*	if(isset($_POST["from_date"], $_POST["to_date"])) {
 		$mysqli = mysqli_connect("localhost","student_11900456","kQ92NTbPGDBI","student_11900456");
 		$query = " SELECT * FROM sensTest WHERE date BETWEEN '".$_POST["from_date"]."' AND '".$_POST["to_date"]."' ";
 		$result = mysqli_query($mysqli, $query);
@@ -13,4 +13,38 @@
 		$json = array('datum' => $datum, 'temp' => $temp, 'hum' => $hum);
 		echo json_encode($json);
 	}
+*/
+
+if(isset($_POST["from_date"], $_POST["to_date"])) {
+	$date1 = $_POST["from_date"];
+	$date2 = $_POST["to_date"];
+
+	$connect = mysqli_connect("localhost","student_11900456","kQ92NTbPGDBI","student_11900456");
+	$sql1 = "SELECT sensoren.sensNaam, sensor_data.waarde, sensor_data.date FROM sensoren JOIN sensor_data ON sensoren.ID = sensor_data.sensorID WHERE sensoren.sensNaam = 'temperature' AND sensor_data.date >= '".$date1."' AND sensor_data.date <= '".$date2."' ";
+	$sql2 = "SELECT sensoren.sensNaam, sensor_data.waarde, sensor_data.date FROM sensoren JOIN sensor_data ON sensoren.ID = sensor_data.sensorID WHERE sensoren.sensNaam = 'humidity' AND sensor_data.date >= '".$date1."' AND sensor_data.date <= '".$date2."' ";
+
+
+	$result1 = mysqli_query($connect,$sql1);
+	$result2 = mysqli_query($connect,$sql2);
+
+
+	while ($data1 = mysqli_fetch_array($result1) ) {
+			$temp[] = $data1['waarde'];
+			$datum[] = $data1['date'];
+	}
+
+	while ($data2 = mysqli_fetch_array($result2) ) {
+			$hum[] = $data2['waarde'];
+	}
+
+	mysqli_close($connect);
+
+	$json = array('datum' => $datum, 'temp' => $temp, 'hum' => $hum);
+
+
+	echo json_encode($json);
+
+
+}
+
 ?>
--- iotFULL/rss.php
+++ iotFULL/rss.php
@@ -1,32 +1,32 @@
-<?php header('Content-type: application/xml');
+<?php
 
 $connect = mysqli_connect("localhost","student_11900456","kQ92NTbPGDBI","student_11900456");
 
 
-echo "<rss version='2.0' xmlns:atom='http://www.w3.org/2005/Atom'>\n";
-echo "<channel>\n";
 
-echo "<title>RSS Feed</title>\n";
-echo "<description>de gemiddelde waardes en de max en min waardes</description>\n";
-echo "<link>http://www.mydomain.com</link>\n";
+$sql1 = "SELECT sensoren.sensNaam, sensor_data.waarde, sensor_data.date, sensor_data.IP FROM sensoren JOIN sensor_data ON sensoren.ID = sensor_data.sensorID WHERE sensoren.sensNaam = 'temperature' ORDER BY sensor_data.date DESC LIMIT 10";
+$query = mysqli_query($connect,$sql1);
 
+header( "Content-type: text/xml");
 
-$sql1 = "SELECT sensoren.sensNaam, sensor_data.waarde, sensor_data.date FROM sensoren JOIN sensor_data ON sensoren.ID = sensor_data.sensorID WHERE sensoren.sensNaam = 'temperature' ORDER BY sensor_data.date DESC LIMIT 1";
-$result1 = $connect->query($sql1);
+echo "<?xml version='1.0' encoding='UTF-8'?>
+<rss version='2.0'>
+<channel>
+<title>titel:test</title>
+<link>/</link>
+<description>RSS van de site</description>
+<language>en-us</language>";
 
-while($row = $result1->fetch_assoc()) {
+while($row = mysqli_fetch_array($query)){
+  $waarde=$row["waarde"];
+  $date=$row["date"];
+  $ip=$row["IP"];
 
-     echo "<item>n";
-         echo "<title>idunno gwn titel</title>\n";
-         echo "<description>$row->newsDesc</description>\n";
-         echo "<pubDate>".date('D, d M Y H:i:s',strtotime($row->newsDate))." GMT</pubDate>\n";
-         echo "<link>http://www.mydomain.com/$row->newsSlug</link>\n";
-         echo "<guid>http://www.mydomain.com/$row->newsSlug</guid>\n";
-         echo "<atom:link href='http://www.mydomain.com/$row->newsSlug' rel='self' type='application/rss+xml'/>\n"
-     echo "</item>\n";
-
+  echo "<item>
+  <title>$waarde</title>
+  <link>$date</link>
+  <description>$ip</description>
+  </item>";
 }
-
-echo "</channel>\n";
-echo "</rss>\n";
+echo "</channel></rss>";
 ?>
